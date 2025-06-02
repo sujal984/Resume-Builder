357cfcf9c2c2a47db71b2c88d57144601d98318c
sujal jain <sujaljain984@gmail.com>
2025-06-02 16:29:24 +0530

    Terminal told to commit

--- src/components/common/ErrorPage.jsx
+++ src/components/common/ErrorPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Result } from "antd";
+import { Helmet } from "react-helmet";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <Helmet>
+        <title> Error 404</title>
+      </Helmet>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+      />
+    </>
+  );
 }
 
 export default ErrorPage;
